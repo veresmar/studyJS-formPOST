67a889228ac40bd07c97a54e284082f401ddb1a3
Maria Veres <veres.maria.dev@gmail.com>
2024-07-28 14:55:46 +0300

    done

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
-const user = {
-  "username": "veres",
-  "password": "696969"
-}
+document.getElementById('authForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const formData = new FormData(this); // Обратите внимание на правильное написание FormData
+    console.log(formData);
+
+    const data = {
+        username: formData.get('fname'), // formData.get - функция, принимающ в кач-ве параметра название поля, возвращ значение, введенное пользователем
+        password: formData.get('lname')
+    };
+    console.log(data)
+   
+    const errorRed = document.getElementById('error')
+
+    fetch("http://84.201.158.229:5000/auth", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data) // Используем данные из формы
+    })
+    .then(response => response.json())
+    .then(function(data) {
+        
+        if (data.status === 'ok') {
+           window.location.href = '/';
+
+        } else {
+            
+           errorRed.textContent=data.description;
+        }
+    })
+    
+});
 
 
-fetch("http://84.201.158.229:8000/auth", {
-    method: "POST",
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(user)
-})
-    .then(data => console.log(data))
+
